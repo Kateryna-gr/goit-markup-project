2d7659e7a8544c17b13fab03f2c18d46e0caef9b
Kateryna-gr <128936446+Kateryna-gr@users.noreply.github.com>
2023-06-06 01:23:13 +0300

    update-9

--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,37 @@
-const swiper = new Swiper('.swiper', {
- direction: 'horizontal',
+// const swiper = new Swiper('.swiper', {
+//  direction: 'horizontal',
+//   loop: true,
+
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
+
+// });
+
+var swiper = new Swiper(".mySwiper", {
   loop: true,
 
-  // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
+    clickable: true,
   },
 
+  // slidesPerView: 2,
+  breakpoints: {
+    // when window width is <= 320px
+    320: {
+      slidesPerView: 1,
+    },
+    // when window width is <= 480px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    // when window width is <= 640px
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 28,
+    },
+  },
 });
